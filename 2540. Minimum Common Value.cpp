232927fde53757fc6ea241918ed747110f06d3e2
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-03-09 09:36:17 +0530

    POTD 9/3/24

---
+++ 2540. Minimum Common Value.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+  int getCommon(vector<int> &nums1, vector<int> &nums2)
+  {
+    int i = 0;
+    int j = 0;
+
+    int n = nums1.size();
+    int m = nums2.size();
+
+    while (i < n && j < m)
+    {
+      if (nums1[i] < nums2[j])
+      {
+        i++;
+      }
+      else if (nums1[i] > nums2[j])
+      {
+        j++;
+      }
+      else
+      {
+        return nums1[i];
+      }
+    }
+    return -1;
+  }
+};
