107c39296912f45a3d32ba2c07b7b83183eaf109
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-03-11 13:26:24 +0530

    POTD 11/3/24

---
+++ 791. Custom Sort String.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+        int n = s.size();
+
+        unordered_map<char, int> mp;
+
+        for(auto x: s){
+            mp[x]++;
+        }
+
+        string ans = "";
+
+        for(int i=0; i<order.size(); i++){
+            if(mp.find(order[i]) != mp.end()){
+                int times = mp[order[i]];
+                while(times--){
+                    ans += order[i];
+                }
+                mp.erase(order[i]);
+            }
+        }
+
+        for(auto x: mp){
+            char ch = x.first;
+            int how_times = x.second;
+            while(how_times--){
+                ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};
