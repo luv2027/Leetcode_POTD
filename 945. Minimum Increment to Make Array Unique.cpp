cae8f1c4e9f0d74fc875124a6e92f2d4b65db724
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-06-14 09:26:16 +0530

    POTD 14/6/24

---
+++ 945. Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        int ans = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i-1] >= nums[i]){
+                ans += (nums[i-1] + 1 - nums[i]);
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
